690f2a30da8b367f74904e7377ce172c3a092db9
NG <guraband@naver.com>
2023-05-24 23:40:08 +0900

    댓글 삭제 구현

--- issue-service/src/main/kotlin/fastcampus/issue/web/CommentController.kt
+++ issue-service/src/main/kotlin/fastcampus/issue/web/CommentController.kt
@@ -4,6 +4,7 @@ import fastcampus.issue.config.AuthUser
 import fastcampus.issue.domain.CommentRequest
 import fastcampus.issue.domain.CommentResponse
 import fastcampus.issue.service.CommentService
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -26,4 +27,15 @@ class CommentController(
         @PathVariable id: Long,
         @RequestBody request: CommentRequest,
     ) = commentService.edit(id, authUser.userId, request)
+
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun delete(
+        authUser: AuthUser,
+        @PathVariable issueId: Long,
+        @PathVariable id: Long,
+    ) {
+        commentService.delete(issueId, id, authUser.userId)
+    }
 }
